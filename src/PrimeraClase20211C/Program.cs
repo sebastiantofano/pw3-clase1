82422fde5331199adfbe675b12567527b39d60d1
Sebastian <sebastiantofano@gmail.com>
2021-04-11 15:46:40 -0300

    Ejercicio realizado v1

--- src/PrimeraClase20211C/Program.cs
+++ src/PrimeraClase20211C/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Logica;
 using System;
+using System.Collections;
 using System.IO;
 
 namespace PrimeraClase20211C
@@ -8,14 +9,29 @@ namespace PrimeraClase20211C
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese un nro:");
-            string num1 = Console.ReadLine();//1
+            //Console.WriteLine("Ingrese un nro:");
+            //string num1 = Console.ReadLine();//1
 
-            Console.WriteLine("Ingrese otro nro:");
-            string num2 = Console.ReadLine();//2
+            //Console.WriteLine("Ingrese otro nro:");
+            //string num2 = Console.ReadLine();//2
             
-            int resultado = Calculadora.Sumar(num1, num2);
-            Console.WriteLine("La suma de ambos nros es: " + resultado);//12
+            //int resultado = Calculadora.Sumar(num1, num2);
+            //Console.WriteLine("La suma de ambos nros es: " + resultado);//12
+
+            ArrayList nombres = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese el nombre de la persona N°{i+1}: ");
+                nombres.Add(Console.ReadLine());
+            }
+
+            nombres.Sort();
+
+            Console.WriteLine("Los nombres de las personas ingresadas son: ");
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+            }
         }
     }
 }
